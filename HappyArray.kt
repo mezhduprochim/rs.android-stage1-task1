a41ba5d7ea8bc8937c396831c460cde8e3ed2cec
mezhduprochim <59843652+mezhduprochim@users.noreply.github.com>
2020-03-29 11:27:57 +0300

    Add files via upload

---
+++ HappyArray.kt
@@ -0,0 +1,11 @@
+package subtask1
+
+class HappyArray {
+
+    // TODO: Complete the following function
+    fun convertToHappy(sadArray: IntArray): IntArray {
+        for i in IntArray
+
+        throw NotImplementedError("Not implemented")
+    }
+}
